3a7f8211d6a4839814f89a91ed08281c028d3c6a
jiongrong fan <1446418561@qq.com>
2018-02-13 22:18:37 +0800

    打算新建tab 组件

---
+++ src/components/tab.vue
@@ -0,0 +1,21 @@
+<!--
+Description
+@authors Your Name (you@example.org)
+@date    2018-02-01 22:41:34
+@version 1.0.0
+-->
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+      
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
